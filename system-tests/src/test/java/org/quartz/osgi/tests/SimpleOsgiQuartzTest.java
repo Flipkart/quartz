94641a2d135c740ece4a0a095beda4edcd8a5036
hhuynh <hhuynh@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2014-07-25 19:27:16 +0000

    update to use slf4j 1.7.7

--- system-tests/src/test/java/org/quartz/osgi/tests/SimpleOsgiQuartzTest.java
+++ system-tests/src/test/java/org/quartz/osgi/tests/SimpleOsgiQuartzTest.java
@@ -42,7 +42,11 @@ public class SimpleOsgiQuartzTest implements JobListener {
 	public Option[] config() {
 		return options(mavenBundle("org.quartz-scheduler", "quartz")
 				.versionAsInProject(), wrappedBundle(maven("c3p0", "c3p0")
-				.versionAsInProject()), OsgiUtil.commonOptions());
+				.versionAsInProject()),
+        mavenBundle("org.slf4j", "slf4j-api").versionAsInProject(),
+        mavenBundle("org.slf4j", "slf4j-log4j12").versionAsInProject().noStart(),
+        mavenBundle("log4j", "log4j").versionAsInProject().noStart(),        
+        OsgiUtil.commonOptions());
 	}
 
 	// note this part of code run in osgi container
